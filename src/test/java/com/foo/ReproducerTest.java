af2c8f6641aad7ad9a4015967f8e3e2b41c15478
Jan Knobloch <jan.knobloch@me.com>
2021-02-19 09:43:51 +0100

    shifted simple main to test..

--- src/test/java/com/foo/ReproducerTest.java
+++ src/test/java/com/foo/ReproducerTest.java
@@ -1,15 +1,21 @@
 package com.foo;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.antwerkz.bottlerocket.BottleRocket;
 import com.antwerkz.bottlerocket.BottleRocketTest;
 import com.github.zafarkhaja.semver.Version;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.junit.Test;
+import dev.morphia.query.experimental.filters.Filters;
+import dev.morphia.query.experimental.updates.UpdateOperators;
 
 public class ReproducerTest extends BottleRocketTest {
     private Datastore datastore;
@@ -19,6 +25,9 @@ public class ReproducerTest extends BottleRocketTest {
         MongoDatabase database = getDatabase();
         database.drop();
         datastore = Morphia.createDatastore(mongo, getDatabase().getName());
+        datastore.getMapper().map(MyParent.class);
+        datastore.getMapper().map(MyChild.class);
+        datastore.ensureIndexes();
     }
 
     @NotNull
@@ -33,8 +42,32 @@ public class ReproducerTest extends BottleRocketTest {
         return BottleRocket.DEFAULT_VERSION;
     }
 
-    @Test
+    @Test(expected=NullPointerException.class)
     public void reproduce() {
+        
+      //ADD PARENT
+        MyParent p = new MyParent(0, "parentName", null);
+        datastore.save(p);
+        //ADD CHILD
+        MyChild c = new MyChild(0, "childName");
+        datastore.save(c);
+        
+        //INSERT CHILD TO PARENT WORKS FINE
+        UpdateResult childInsertResult = datastore.find(MyParent.class).filter(Filters.eq("_id", 0L)).update(UpdateOperators.set("child", c)).execute();
+       
+        //FIND CHILD
+        MyChild childFound = datastore.find(MyChild.class).filter(Filters.eq("_id", 0L)).first();
+       //FIND PARENT
+        MyParent parentFound = datastore.find(MyParent.class).filter(Filters.eq("_id", 0L)).first();
+      
+        
+      //UNSET CHILD CAUSES NPE USING THE SAME FILTER FIND AS ABOVE
+        
+        Assert.assertThrows(NullPointerException.class,
+                ()->{
+                    datastore.find(MyParent.class).filter(Filters.eq("_id", 0L)).update(UpdateOperators.unset("child")).execute();
+                }); 
+        
     }
 
 }
